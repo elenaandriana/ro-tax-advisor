019eed6a88f2462ac96e0e06a499ae8ffc851166
Elena Andriana <elenaadrianavoina@yahoo.com>
2026-08-07 01:09:20 +0300

    Add HTML preprocessing and Docling HybridChunker pipeline

--- src/ingestion/parse.py
+++ src/ingestion/parse.py
@@ -1,8 +1,28 @@
+from unittest import result
+
 from docling.document_converter import DocumentConverter
+from docling.chunking import HybridChunker
 
 def chunk(input_file: str):
-    """
+    converter = DocumentConverter()
+    result = converter.convert(input_file)
+    
+    text = result.document.export_to_markdown()
+
+    chunker = HybridChunker()
+    chunks_list = list(chunker.chunk(result.document))
+
+    
+    print(f"Number of chunks: {len(chunks_list)}")
+    f = open("output.txt", "w", encoding="utf-8")
+    f.write(text)
+    f.close()
+   
+    
+    """""
     @param input_file HTML input file
     @return Tuple of chunks and their metadata
     """
-    pass
+
+
+chunk("Legea nr.227_2015.html")
